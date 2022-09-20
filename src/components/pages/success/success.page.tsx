ddd26dc131e89158c78b70548b5d20126a93fba9
Rastsislau Kharlanau <rastsislau.kharlanau@innowise-group.com>
2022-09-20 13:53:41 +0300

    feat: success page

--- src/components/pages/success/success.page.tsx
+++ src/components/pages/success/success.page.tsx
@@ -1,5 +1,29 @@
+import { observer } from 'mobx-react-lite'
+import { Navigate } from 'react-router-dom'
+import { Typography } from '@mui/material'
+import { answersService } from '@services/answers'
+import { AppRoutes } from '@constants/app-routes.constant'
+import { Answer } from '@molecules/answer'
+import * as Styled from './success.styles'
+
 const Success = () => {
-  return <div>success</div>
+  const { answers$ } = answersService
+
+  if (!answers$.length) {
+    return <Navigate to={AppRoutes.Survey} />
+  }
+
+  return (
+    <Styled.Success>
+      <Typography variant="h2">Thank you!</Typography>
+      <Typography variant="body1" sx={{ mb: 5 }}>
+        Preview of questions and answers.
+      </Typography>
+      {answers$.map(({ questionId, question, answer }) => (
+        <Answer key={questionId} question={question} answer={answer} />
+      ))}
+    </Styled.Success>
+  )
 }
 
-export default Success
+export default observer(Success)
