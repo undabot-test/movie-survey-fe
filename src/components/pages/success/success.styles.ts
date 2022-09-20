849643ebed402462af0d609f50f4383b9c448f7d
Rastsislau Kharlanau <rastsislau.kharlanau@innowise-group.com>
2022-09-20 13:53:41 +0300

    feat: success page

---
+++ src/components/pages/success/success.styles.ts
@@ -0,0 +1,9 @@
+import { styled } from '@mui/material'
+
+export const Success = styled('div')({
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  marginTop: 32,
+  marginBottom: 32,
+})
