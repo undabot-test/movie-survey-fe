f0f49f7bcb82ac7dd83ebd3ea63039f1f6f8e0b5
Rastsislau Kharlanau <rastsislau.kharlanau@innowise-group.com>
2022-09-20 17:22:32 +0300

    style: 404 page

--- src/components/pages/not-found/not-found.page.tsx
+++ src/components/pages/not-found/not-found.page.tsx
@@ -1,5 +1,23 @@
+import { useNavigate } from 'react-router-dom'
+import { Button, Typography } from '@mui/material'
+import { AppRoutes } from '@constants/app-routes.constant'
+import * as Styled from './not-found.styles'
+
 const NotFound = () => {
-  return <div>404</div>
+  const navigate = useNavigate()
+
+  const handleClick = () => {
+    navigate(AppRoutes.Survey)
+  }
+
+  return (
+    <Styled.Page maxWidth="sm">
+      <Typography variant="h1">404</Typography>
+      <Button variant="outlined" color="primary" sx={{ mt: 2 }} onClick={handleClick}>
+        Homepage
+      </Button>
+    </Styled.Page>
+  )
 }
 
 export default NotFound
