380e7de8c2c62a42c230cb989e3d098f6787809a
Rastsislau Kharlanau <rastsislau.kharlanau@innowise-group.com>
2022-09-22 01:40:43 +0300

    fix: worker registration

--- src/helpers/register-worker.helper.ts
+++ src/helpers/register-worker.helper.ts
@@ -1,3 +1,11 @@
-export const registerWorker = () => {
-  navigator.serviceWorker.register('/worker.js', { scope: '/' })
+export const registerWorker = async () => {
+  const registration = await navigator.serviceWorker.register('/worker.js', { scope: '/' })
+  const worker = registration.installing
+
+  if (!worker) {
+    return
+  }
+  worker.onstatechange = () => {
+    worker.state === 'installed' && window.location.reload()
+  }
 }
