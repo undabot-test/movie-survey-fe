ee643691a7edd3526cec1b5e20c4873afba441a9
Rastsislau Kharlanau <rastsislau.kharlanau@innowise-group.com>
2022-09-22 19:30:09 +0300

    style: theming

--- src/components/app/app.theme.ts
+++ src/components/app/app.theme.ts
@@ -1,11 +1,30 @@
 import { createTheme } from '@mui/material'
 
 export const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#fc6e51',
+    },
+  },
   components: {
     MuiOutlinedInput: {
       styleOverrides: {
         input: {
           boxShadow: '0 0 0 30px #fff inset !important',
+        },
+      },
+    },
+    MuiButton: {
+      styleOverrides: {
+        containedPrimary: {
+          color: '#fff',
+        },
+      },
+    },
+    MuiRating: {
+      styleOverrides: {
+        iconFilled: {
+          color: '#fc6e51',
         },
       },
     },
